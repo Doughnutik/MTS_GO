e7b68c85bf4414420e3f5c9bba791fd9bc331ee6
Doughnutik <artemgukovwork@yandex.ru>
2024-11-28 01:35:50 +0300

    реализовал 2 генератора

---
+++ hw1/internal/idGenerator/idGenerator.go
@@ -0,0 +1,48 @@
+package idgenerator
+
+import "hw1/internal/book"
+
+type Generator interface {
+	Generate(book book.Book) uint
+}
+
+type HashGenerator struct {
+	mod  uint
+	base uint
+}
+
+type BasicGenerator struct {
+	id uint
+}
+
+func NewDefaultHashGenerator() HashGenerator {
+	return HashGenerator{1e9 + 7, 3001}
+}
+
+func NewHashGenerator(mod, base uint) HashGenerator {
+	return HashGenerator{mod, base}
+}
+
+func NewBasicGenerator() BasicGenerator {
+	return BasicGenerator{0}
+}
+
+func (hashgen *HashGenerator) Generate(book book.Book) uint {
+	var hash uint
+	title := book.GetTitle()
+	author := book.GetAuthor()
+	pages := book.GetPages()
+	for i := 0; i < len(title); i++ {
+		hash = (hash + uint(title[i])*hashgen.base) % hashgen.mod
+	}
+	for i := 0; i < len(author); i++ {
+		hash = (hash + uint(author[i])*hashgen.base) % hashgen.mod
+	}
+	hash = (hash + pages*hashgen.base) % hashgen.mod
+	return hash
+}
+
+func (basicgen *BasicGenerator) Generate(book book.Book) uint {
+	basicgen.id++
+	return basicgen.id
+}
